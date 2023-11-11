2649c1ad7f23d709704b2b927a0609bbf7895fcf
jana-medina <jana.medina@granteam.pl>
2023-11-12 00:18:04 +0100

    add endpoints for creating short link and redirecting to long link

--- webhelpers/urlshortener/urls.py
+++ webhelpers/urlshortener/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from urlshortener.views import LinkPairView, link_redirect
 
 urlpatterns = [
-    # path('', pass),
+    path('link-pair', LinkPairView.as_view()),
+    path('my-site/<str:code>', link_redirect),
 ]
--- webhelpers/urlshortener/views.py
+++ webhelpers/urlshortener/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+#django
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
+# rest_framework
+from rest_framework import status, generics, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# project
+from urlshortener.models import LinkPair
+from urlshortener.serializers import LinkPairSerializer
 
-# Create your views here.
+
+class LinkPairView(generics.GenericAPIView, mixins.CreateModelMixin):
+    queryset = LinkPair
+    serializer_class = LinkPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+@api_view(['GET'])
+def link_redirect(request, code):
+    try:
+        link_pair = LinkPair.objects.get(short_link=code)
+    except ObjectDoesNotExist:
+        return Response({'message': 'This link does not exist'}, status.HTTP_204_NO_CONTENT)
+    redirect_address = link_pair.long_link
+    return redirect(redirect_address)
