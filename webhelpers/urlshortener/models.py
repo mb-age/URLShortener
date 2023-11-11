0741647a98230535483aea2ed41aa681be2dace1
jana-medina <jana.medina@granteam.pl>
2023-11-12 00:13:27 +0100

    change model field, create migration

--- webhelpers/urlshortener/models.py
+++ webhelpers/urlshortener/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class LinkPair(models.Model):
     long_link = models.URLField()
-    short_link = models.CharField(max_length=6)
+    short_link = models.CharField(max_length=6, null=True, unique=True)
     created_dt = models.DateTimeField(auto_now_add=True)
     is_custom = models.BooleanField(default=False)
