98f116e807f6d1abb7973a10e81280ac2124ef84
jana-medina <jana.medina@granteam.pl>
2023-11-12 00:11:37 +0100

    add type hinting and documentation to helpers func, fix import

--- webhelpers/urlshortener/helpers.py
+++ webhelpers/urlshortener/helpers.py
@@ -2,9 +2,17 @@
 
 import random
 import string
-from webhelpers.webhelpers.settings import LINK_LENGTH
+from webhelpers.settings import LINK_LENGTH
 
 
-def link_generator(length=LINK_LENGTH):
+def link_generator(length: int = LINK_LENGTH) -> str:
+    """
+    The function generates a random string of characters with a specified length.
+
+    :param length: The length parameter is the length of the generated link. It is an optional parameter
+    with a default value of LINK_LENGTH
+    :return: a randomly generated string of characters with a length specified by the `length`
+    parameter.
+    """
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
