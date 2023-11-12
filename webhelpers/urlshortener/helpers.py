f0f68e3446883c19aa752dbc44824ce3fb80978a
jana-medina <jana.medina@granteam.pl>
2023-11-12 20:37:59 +0100

    edit alias generator function

--- webhelpers/urlshortener/helpers.py
+++ webhelpers/urlshortener/helpers.py
@@ -2,17 +2,25 @@
 
 import random
 import string
-from webhelpers.settings import LINK_LENGTH
+from webhelpers.settings import ALIAS_LENGTH
+from urlshortener.models import LinkPair
 
 
-def link_generator(length: int = LINK_LENGTH) -> str:
+def alias_generator(length: int = ALIAS_LENGTH) -> str:
     """
-    The function generates a random string of characters with a specified length.
+    The function generates a random alias of characters with a specified length and checks if it
+    already exists in the database, generating a new one if necessary.
 
-    :param length: The length parameter is the length of the generated link. It is an optional parameter
-    with a default value of LINK_LENGTH
-    :return: a randomly generated string of characters with a length specified by the `length`
-    parameter.
+    :param length: The "length" parameter is an optional parameter that specifies the length of the
+    generated alias. If no value is provided, it will default to the value of the constant
+    "ALIAS_LENGTH"
+    :type length: int
+    :return: a randomly generated string of characters.
     """
     characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(length))
+    alias = ''.join(random.choice(characters) for _ in range(length))
+
+    while LinkPair.objects.filter(alias=alias).exists():
+        alias = alias_generator()
+
+    return alias
