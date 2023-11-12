c95dd519e1f75a412bb612d90eae67db6ae40305
jana-medina <jana.medina@granteam.pl>
2023-11-12 20:40:18 +0100

    replace create alias logic, add alias validator

--- webhelpers/urlshortener/serializers.py
+++ webhelpers/urlshortener/serializers.py
@@ -1,6 +1,5 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from urlshortener.models import LinkPair
-from urlshortener.helpers import link_generator
 
 
 class LinkPairSerializer(serializers.ModelSerializer):
@@ -8,13 +7,9 @@ class LinkPairSerializer(serializers.ModelSerializer):
         model = LinkPair
         fields = '__all__'
 
-    def create(self, validated_data):
-        short_link = self.create_short_link()
-        validated_data['short_link'] = short_link
-        return super().create(validated_data)
-
-    def create_short_link(self):
-        short_link = link_generator()
-        while LinkPair.objects.filter(short_link=short_link).exists():
-            short_link = link_generator()
-        return short_link
+    def validate_alias(self, data):
+        if LinkPair.objects.filter(alias=data).exists():
+            raise exceptions.ValidationError({'alias': 'Alias is not available.'})
+        if len(data) < 5:
+            raise exceptions.ValidationError({'alias': 'The Alias must be at least 5 characters.'})
+        return data
--- webhelpers/urlshortener/views.py
+++ webhelpers/urlshortener/views.py
@@ -1,4 +1,4 @@
-#django
+# django
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect
 # rest_framework
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 # project
 from urlshortener.models import LinkPair
 from urlshortener.serializers import LinkPairSerializer
+from urlshortener.helpers import alias_generator
 
 
 class LinkPairView(generics.GenericAPIView, mixins.CreateModelMixin):
@@ -15,14 +16,25 @@ class LinkPairView(generics.GenericAPIView, mixins.CreateModelMixin):
     serializer_class = LinkPairSerializer
 
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        if request.data.get('alias'):
+            request.data['is_custom'] = True
+        else:
+            alias = alias_generator()
+            request.data['alias'] = alias
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 @api_view(['GET'])
-def link_redirect(request, code):
+def link_redirect(request, alias):
     try:
-        link_pair = LinkPair.objects.get(short_link=code)
+        link_pair = LinkPair.objects.get(alias=alias)
     except ObjectDoesNotExist:
         return Response({'message': 'This link does not exist'}, status.HTTP_204_NO_CONTENT)
-    redirect_address = link_pair.long_link
-    return redirect(redirect_address)
+    target_url = link_pair.url
+
+    return redirect(target_url)
