a6c284c5221a77212efc1bde4c81c8bbe3e80e63
jana-medina <jana.medina@granteam.pl>
2023-11-12 23:28:25 +0100

    change url

--- webhelpers/urlshortener/urls.py
+++ webhelpers/urlshortener/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from urlshortener.views import LinkPairView, link_redirect, request_count
 
 urlpatterns = [
-    path('url/create', LinkPairView.as_view()),
+    path('url', LinkPairView.as_view()),
     path('url/<str:alias>', link_redirect),
     path('url/<str:alias>/request-count', request_count),
 ]
