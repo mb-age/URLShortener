95a620da93669c35ef999127ddfa45805204f354
jana-medina <jana.medina@granteam.pl>
2023-11-16 20:50:34 +0100

    edit tests

--- webhelpers/urlshortener/tests.py
+++ webhelpers/urlshortener/tests.py
@@ -22,7 +22,7 @@ class TestAPI(APITestCase):
         self.assertEqual(result_get['url'], 'https://www.google.com')
         self.assertEqual(result_get['alias'], 'dJty5167')
         self.assertIsInstance(result_get, dict)
-        self.assertEqual(len(result_get), 6)
+        self.assertEqual(len(result_get), 8)
 
     def test_link_pair_create(self):
         """
@@ -46,7 +46,7 @@ class TestAPI(APITestCase):
         self.assertEqual(len(result_create['alias']), 8)
         self.assertIsInstance(result_create, dict)
         self.assertEqual(LinkPair.objects.all().count(), 2)
-        self.assertEqual(len(result_create), 6)
+        self.assertEqual(len(result_create), 8)
         self.assertEqual(
             result_create_with_alias['url'],
             'https://www.w3schools.com/',
